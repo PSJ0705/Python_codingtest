ad1c20cea63001538c6af4b7cbdb67b5471c4254
PSJ0705 <67884516+PSJ0705@users.noreply.github.com>
2026-01-06 21:24:22 +0900

    [level 0] Title: 수 조작하기 1, Time: 1.08 ms, Memory: 9.21 MB -BaekjoonHub

---
+++ Python3/프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.py
@@ -0,0 +1,10 @@
+def solution(n, control):
+    w_count = control.count('w')
+    s_count = control.count('s')
+    d_count = control.count('d')
+    a_count = control.count('a')
+    
+    num = n + (1 * w_count) + (-1 * s_count) + (10 * d_count) + (-10 * a_count)
+    return num
+    
+
