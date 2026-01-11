20c2802dfeb1bd44d398b3b076efbf6bc64794a0
PSJ0705 <67884516+PSJ0705@users.noreply.github.com>
2026-01-11 14:55:22 +0900

    [level 1] Title: 시저 암호, Time: 1.54 ms, Memory: 9.18 MB -BaekjoonHub

---
+++ Python3/프로그래머스/1/12926. 시저 암호/시저 암호.py
@@ -0,0 +1,16 @@
+from string import ascii_lowercase
+from string import ascii_uppercase
+
+def solution(s, n):
+    answer = ''
+    lower_list = ascii_lowercase * 2
+    upper_list = ascii_uppercase * 2
+    
+    for i in s:
+        if i in lower_list:
+            answer += lower_list[lower_list.index(i) + n]
+        elif i in upper_list:
+            answer += upper_list[upper_list.index(i) + n]
+        else:
+            answer += ' '
+    return answer
