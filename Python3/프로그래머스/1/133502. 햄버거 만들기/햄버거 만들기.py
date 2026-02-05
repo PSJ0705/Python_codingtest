bed314275a4f8db2f164b0be0fbc23fa4ed8e1bd
PSJ0705 <67884516+PSJ0705@users.noreply.github.com>
2026-02-05 21:49:50 +0900

    [level 1] Title: 햄버거 만들기, Time: 205.60 ms, Memory: 25.6 MB -BaekjoonHub

--- Python3/프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.py
+++ Python3/프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.py
@@ -2,18 +2,12 @@ def solution(ingre):
     answer = 0
     sample = [1,2,3,1]
     box = []
-    i = 0
     
-    while i <= len(ingre):
-        j = i
-        box.append(ingre[i:i+4])
-        if sample in box:
-            del ingre[i:i+4]
-            box = []
+    for i in ingre:
+        box.append(i)
+        if box[-4:] == [1,2,3,1]:
             answer += 1
-            i = j - 4
-            continue
-        else:
-            box = []
-            i += 1
+            for j in range(4):
+                box.pop()
+
     return answer
